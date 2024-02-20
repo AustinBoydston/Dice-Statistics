85da79af6779abf908cb8f74e9491ca732419342
Austin Boydston <43730698+AustinBoydston@users.noreply.github.com>
2024-02-20 14:05:35 -0600

    Added user input, code calculates odds for all possible values
    User input now accepts any number of dice all consisting of any number of faces (all dice are assumed to have the same amount of faces). Code now calculates the odds of all possible throw values, and determines the average throw value.

--- dicestats.py
+++ dicestats.py
@@ -6,14 +6,25 @@ SampleSize = 0
 acceptInputSample = False
 
 NumberOfDiceString = ""
-NumberOfDice = 0
+NumberOfDice = 2
 acceptInputNumberOfDice = False
 
 NumberOfFacesString = ""
 NumberOfFaces = 6
 acceptInputNumberOfFaces = False
 
-#get the sample size
+
+DiceThrows = []
+
+
+
+def SumArray(dataArray):
+    sum = 0
+    for a in dataArray:
+        sum = sum + a
+    return sum
+
+#get the sample size from the user.
 while(True):
     try:
         print("Enter a sample size. It must be an integer. (>=10000 recomended)")
@@ -21,63 +32,92 @@ while(True):
         SampleSize = int(SampleSizeString)
         acceptInputSample = True
     except TypeError:
-        print("You did not enter an integer, try again")
+        print("You did not enter an integer, try again.")
     if acceptInputSample:
         if SampleSize <= 0:
             print("You entered a non-posative value, re-run the code and enter a posative integer.")
             exit()
-        elif SampleSize < 5000:
+        elif SampleSize < 1000:
             print("This is a small sample, larger samples are more statistically accurate")
         break
 
-#Get the number of dice
+#Get the number of dice from the user.
 while(True):
     try:
         print("Enter the amount of dice. This must be greater than zero.")
         NumberOfDiceString = input()
-        SampleSize = int(SampleSizeString)
-        acceptInput = True
+        NumberOfDice = int(NumberOfDiceString)
+        acceptInputNumberOfDice = True
     except TypeError:
         print("You did no enter an integer, try again")
-    if acceptInput:
-        if SampleSize <= 0:
+    if acceptInputNumberOfDice:
+        if NumberOfDice <= 0:
             print("You entered a non-posative value, re-run the code and enter a posative integer.")
             exit()
-        elif SampleSize < 5000:
-            print("This is a small sample, larger samples are more statistically accurate")
         break
 
-# Check if the input is a number
+
+#Get the number of dice faces from the user.
+while(True):
+    try:
+        print("Enter the number of faces on each dice. This must be 2 or greater.")
+        NumberOfFacesString = input()
+        NumberOfFaces = int(NumberOfFacesString)
+        acceptInputNumberOfFaces = True
+    except TypeError:
+        print("You did no enter an integer, try again")
+    if acceptInputNumberOfFaces:
+        if NumberOfFaces < 2:
+            print("You entered a number less than 2, re-run the code and enter a posative integer greater than or equal to 2.")
+            exit()
+        break
+
+
+
 
 #declare our variables
 data = []
-datatemp = []
 
 
-countOf53 = 0
-countOf2 = 0
+#initialize counts for the given amount of possible a value outcomes on each dice
+counts = []
+ResultsInitCounter = 0
+while(ResultsInitCounter <= (NumberOfFaces * NumberOfDice)):
+    counts.append(0)
+    ResultsInitCounter = ResultsInitCounter + 1
+
+
 counter = 0
-sum = 0
+diceSum = 0
 
+#throw the dice SampleSize amount of times
 while(counter < SampleSize):
-    num1 = random.randint(1, 6)
-    num2 = random.randint(1, 6)
-
-    if(num1 + num2 == 12):
-        countOf2 = countOf2 + 1
-
-    if((num1 == 3 and num2 == 5) or (num1 == 5 and num2 == 3)):
-        datatemptemp = []
-        datatemptemp.append(num1)
-        datatemptemp.append(num2)
-        datatemp.append(datatemptemp)
-        countOf53 = countOf53 + 1
+    Dice = []    
+    diceSum = 0
+    makeDiceCounter = 0
     
-    sum = sum + num1 + num2
+    while(makeDiceCounter < NumberOfDice):
+        Dice.append(random.randint(1, NumberOfFaces))
+        DiceThrows.append(Dice[makeDiceCounter])
+        makeDiceCounter = makeDiceCounter + 1
+    
+    
+    
+    diceSum = SumArray(Dice)
+    counts[diceSum] = counts[diceSum] + 1
+    
+    
+    
+     
+    
     counter = counter + 1
-    data.append(num1 + num2)
-#print(data)
-print(datatemp)
-print("The average throw is: ", sum / SampleSize)
-print("The chance of throwing doubles is: ",6 * countOf2/SampleSize)
-print("The chance of throwing a 5 and a 3 is: ", countOf53/SampleSize)
+    
+
+
+# output section
+printCounter = 0
+while(printCounter <= NumberOfDice * NumberOfFaces):
+    print("The probability of ", printCounter, " being thrown is: ", counts[printCounter] / SampleSize)
+    printCounter = printCounter + 1
+print("The average throw is: ", SumArray(DiceThrows) / SampleSize)
+
